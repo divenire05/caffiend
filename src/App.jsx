e896782e82861178cdd874fc03ecc6ff30a927e0
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-11 12:25:33 -0500

    setting up layout and exporting components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import CoffeeForm from "./components/CoffeeForm"
+import Hero from "./components/Hero"
+import Layout from "./components/Layout"
+
 function App() {
 
+  const isAuthenticated = false
+
   return (
-    <>
-      
-    </>
+    <Layout>
+      <Hero />
+      <CoffeeForm/>
+    </Layout>
   )
 }
 
