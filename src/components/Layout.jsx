12bd5b3c79d99200990ba812a3bab4ede4fc043d
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-11 13:07:29 -0500

    adding header and footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,22 @@ export default function Layout(props) {
     const {children} = props
 
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1 className="text-gradient">CAFFIEND</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i class="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
 
     const footer = (
-        <footer></footer>
+        <footer>
+            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://github.com/divenire05">divenire05</a> using the <a target="_blank" href="https://www.fantacss.smoljames.com">FantaCSS</a> design library.</p>
+        </footer>
     )
 
     return (
